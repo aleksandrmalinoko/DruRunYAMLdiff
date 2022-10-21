2739be7ae12f58daab2e1c7c6e92820b6ba22067
aleksandrmalinko <aleksandrmalinoko@gmail.com>
2022-10-21 14:12:00 +0300

    removed and changed elements

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import sys
 import argparse
 import re
 ignored_list = ["metadata.selfLink", "metadata.resourceVersion", "metadata.uid", "metadata.creationTimestamp",
-                "metadata.managedFields", "status", "lifecycle", "args"]
+                "metadata.managedFields", "status", "lifecycle", "args", "apiVersion", "terminationMessagePath",
+                "terminationMessagePolicy", "scheme", "spec.template.spec.restartPolicy", "dnsPolicy", "schedulerName", "spec.clusterIP",
+                "spec.sessionAffinity", "spec.template.spec.dnsPolicy"]
 
 
 def yaml_printer(yaml_dict, indent_count):
@@ -62,7 +64,7 @@ def dictionary_item_removed(keys):
     print(f" {global_indent * 2 * '_'}")
     print(f"|Было{(global_indent - 5) * ' '}|Стало{(global_indent - 5) * ' '}|")
     print(f" {global_indent * 2 * '_'}")
-    for elem in keys[0:-2]:
+    for elem in keys[:-2]:
         left_print = f"|{yaml_indent * '    '}{elem}:"
         print(left_print, end="")
         print(f"{(global_indent - len(left_print)) * ' '}|{global_indent * ' '}|")
@@ -75,8 +77,9 @@ def dictionary_item_removed(keys):
 
 
 def dictionary_item_changed(keys, old_value, new_value):
-    global_indent = (len(keys) + 2) * 4 + max(len(str(old_value)),
-                                             len(str(new_value)))
+    global_indent = len(keys) * 4 + max((max(len(str(old_value)),
+                                                    len(str(new_value))),
+                                                max(len(str(x)) for x in keys)))
     yaml_indent = 0
     print(f" {global_indent * 2 * '_'}")
     diff_type = "Изменилось значение"
@@ -99,6 +102,30 @@ def dictionary_item_changed(keys, old_value, new_value):
     left_space_print = f"{(global_indent - len(left_print)) * ' '}|"
     right_print = f"{yaml_indent * '    '}{new_value}"
     right_space_print = f"{(global_indent - len(right_print)) * ' '}|"
+    print(left_space_print, right_print, right_space_print, sep="")
+    print(f"|{global_indent * 2 * '_'}|")
+
+
+def dictionary_item_added(keys):
+    global_indent = len(str(keys))
+    yaml_indent = 0
+    print(f" {global_indent * 2 * '_'}")
+    diff_type = "Добавился элемент"
+    tmp_left_indent = global_indent - len(diff_type) // 2
+    tmp_right_indent = global_indent * 2 - (tmp_left_indent + len(diff_type))
+    print(f"|{tmp_left_indent * ' '}{diff_type}{tmp_right_indent * ' '}|")
+    print(f" {global_indent * 2 * '_'}")
+    print(f"|Было{(global_indent - 5) * ' '}|Стало{(global_indent - 5) * ' '}|")
+    print(f" {global_indent * 2 * '_'}")
+    for elem in keys[:-2]:
+        left_space_print = f"|{global_indent * ' '}|"
+        right_print = f"{yaml_indent * '    '}{elem}:"
+        right_space_print = f"{(global_indent - len(right_print) - 1) * ' '}|"
+        print(left_space_print, right_print, right_space_print, sep="")
+        yaml_indent += 1
+    left_space_print = f"|{global_indent * ' '}|"
+    right_print = f"{yaml_indent * '    '}{keys[-2]}: {keys[-1]}"
+    right_space_print = f"{(global_indent - len(right_print) - 1) * ' '}|"
     print(left_space_print, right_print, right_space_print, sep="")
     print(f"|{global_indent * 2 * '_'}|")
 
@@ -146,7 +173,31 @@ def yaml_diff_printer(diff):
             new_value = second_part[1]
             dictionary_item_changed(changed_key, old_value, new_value)
         elif diff_element[0] == 'add':
-            pass
+            changed_keys_list = []
+            changed_key = []
+            if isinstance(diff_element[1], str):
+                first_part = diff_element[1].split(".")
+                if isinstance(first_part, str):
+                    changed_key.append(first_part)
+                else:
+                    changed_key.extend(first_part)
+            second_part = diff_element[2]
+            temp_changed_key = list(changed_key)
+            for key in second_part:
+                changed_key = list(temp_changed_key)
+                changed_key.append(key[0])
+                if isinstance(key[1], str):
+                    changed_key.append(key[1])
+                    changed_keys_list.append(changed_key)
+                elif isinstance(key[1], dict):
+                    temp_changed_key = list(changed_key)
+                    for dict_element in key[1]:
+                        changed_key = list(temp_changed_key)
+                        changed_key.append(dict_element)
+                        changed_key.append(key[1][dict_element])
+                        changed_keys_list.append(changed_key)
+            for aaaa_changed_key in changed_keys_list:
+                dictionary_item_removed(aaaa_changed_key)
 
 
 def yaml_diff_printer_old_2(ddiff):
@@ -272,8 +323,12 @@ def cycle_file_differ(backup_location, dryrun_location, diff_location):
                 except yaml.YAMLError as exc:
                     print(exc)
 
-            with open(f"{backup_location}/{backup_file}", "r") as stream:
+            with open(f"{backup_location}/{backup_file}", "w+") as stream:
                 try:
+                    # temp_backup = stream.read()
+                    # temp_backup = temp_backup.replace("application-secure.yml: |-", "application-secure.yml:")
+                    # temp_backup = temp_backup.replace("application.yml: |-", "application.yml:")
+                    # stream.write(temp_backup)
                     backup_yaml = yaml.unsafe_load(stream)
                 except yaml.YAMLError as exc:
                     print(exc)
@@ -293,18 +348,20 @@ if __name__ == "__main__":
     parser.add_argument('backup', type=str, help='Folder that contains all backup files')
     parser.add_argument('diff', type=str, help='Empty folder for diff files')
     args = parser.parse_args()
-    # dryrun_file = os.listdir(args.dryrun)[0]
-    # with open(f"{args.dryrun}/{dryrun_file}", "r") as stream:
-    #     dryrun = stream.read()
-    #     dryrun = re.sub(r"#.+\n", "", dryrun)
-    #     dryrun = dryrun.split('---')
-    #
-    # for elem in dryrun:
-    #     tmp_manifest = yaml.unsafe_load(elem)
-    #     original_stdout = sys.stdout
-    #     with open(f"dryrun/{tmp_manifest['kind'].lower()}-{tmp_manifest['metadata']['name']}.yaml", 'w') as out_file:
-    #         sys.stdout = out_file  # Change the standard output to the file we created.
-    #         print(yaml.dump(tmp_manifest))
-    #         sys.stdout = original_stdout
+    dryrun_file = os.listdir(args.dryrun)[0]
+    with open(f"{args.dryrun}/{dryrun_file}", "r") as stream:
+        dryrun = stream.read()
+        dryrun = dryrun.replace("application-secure.yml: |-", "application-secure.yml:")
+        dryrun = dryrun.replace("application.yml: |-", "application.yml:")
+        dryrun = re.sub(r"#.+\n", "", dryrun)
+        dryrun = dryrun.split('---')
+
+    for elem in dryrun:
+        tmp_manifest = yaml.unsafe_load(elem)
+        original_stdout = sys.stdout
+        with open(f"dryrun/{tmp_manifest['kind'].lower()}-{tmp_manifest['metadata']['name']}.yaml", 'w') as out_file:
+            sys.stdout = out_file  # Change the standard output to the file we created.
+            print(yaml.dump(tmp_manifest))
+            sys.stdout = original_stdout
 
     cycle_file_differ(args.backup, args.dryrun, args.diff)
